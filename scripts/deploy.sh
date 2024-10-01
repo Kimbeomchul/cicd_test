21ae0625f548722b0e25f2db168df4a8f3d50c16
beomchul kim <2017253083@yonsei.ac.kr>
2024-10-02 00:02:04 +0900

    fix deploy sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,15 @@
 
 BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "#### 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
+echo "#### JAR NAME: $JAR_NAME" >> /home/ubuntu/deploy.log
 
-echo "#### 파일 복사" >> /home/ubuntu/deploy.log
+echo "#### CP START" >> /home/ubuntu/deploy.log
 DEPLOY_PATH=/home/ubuntu/app/build/libs/
 cp $BUILD_JAR $DEPLOY_PATH
+echo "#### CP FIN $DEPLOY_PATH" >> /home/ubuntu/deploy.log
 
-echo "#### pid" >> /home/ubuntu/deploy.log
 sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "#### 배포"    >> /home/ubuntu/deploy.log
-echo "#### $DEPLOY_JAR $JAR_NAME " >> /home/ubunru/deploy.log
+echo "#### DEPLOY $DEPLOY_JAR $JAR_NAME " >> /home/ubunru/deploy.log
 nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2> /home/ubuntu/deploy_err.log &
